0a96ebcef6d12f70d35b70114968fdf6cf566212
ahmed flutter <gghh1991h@gmail.com>
2021-10-05 11:55:30 +0300

    run from kayshourt jast that

--- bin/test_dart.dart
+++ bin/test_dart.dart
@@ -1,24 +1,15 @@
-import 'package:http/http.dart' as http;
-import 'dart:convert';
 
-import 'package:test_dart/smple_fetcch_json.dart';
-import 'package:test_dart/work_with_api.dart';
+import 'package:hive/hive.dart';
+import 'package:test_dart/hive.dart';
 
-main() async {
-  await postData('http://localhost:1337/Games');
+
+void main() async {
+  Hive.init('F:\Flutter Run\flutter live\test_dart\ahmed');
+  hive_DB();
+// print('object');
+
+  // await postData('http://localhost:1337/Games');
 
   // await updateData('http://localhost:1337/Games/6159e86c6248be24bc1bf922');
   // await DeleteData('http://localhost:1337/Games/6159e86c6248be24bc1bf922');
-}
-
-Future<http.Response> createAlbum(String title) {
-  return http.post(
-    Uri.parse('https://jsonplaceholder.typicode.com/albums'),
-    headers: <String, String>{
-      'Content-Type': 'application/json; charset=UTF-8',
-    },
-    body: jsonEncode(<String, String>{
-      'title': title,
-    }),
-  );
 }
---
+++ lib/hive.dart
@@ -0,0 +1,6 @@
+void hive_DB() {
+  print('ahmed!!!!');
+}
+
+
+// https://stackoverflow.com/questions/52064568/flutter-trouble-launching-app-for-debug-on-macos
